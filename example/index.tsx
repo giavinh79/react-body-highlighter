7121f8acf9fcbf4c8cb161a2a4257fc594543593
GV79 <glam01@uoguelph.ca>
2020-06-24 15:48:49 -0400

    fix for types

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Model from '../dist/index';
-import ModelTest from '../src/index';
 
 const App = () => {
   return (
@@ -12,9 +11,8 @@ const App = () => {
           { name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'] },
           { name: 'Tricep Pushdown', muscles: ['triceps'] },
         ]}
-        onClick={(exercise: object) => alert(JSON.stringify(exercise))}
+        onClick={exercise => alert(JSON.stringify(exercise))}
       />
-      <ModelTest />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Svg, Polygon } from './styles';
 import { CSSProperties } from 'react';
 
